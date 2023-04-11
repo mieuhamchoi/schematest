ff9d3f1d251d6c87abc7ce414336efaada53d588
Nguyen Phuoc <mieuhamchoi@gmail.com>
2023-04-11 19:24:54 +0700

    init project

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div className="bg-white border-gray-200 dark:bg-gray-900 text-base">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <h1>Đây là about page</h1>
+        </div>
+    </div>
+  )
+}
